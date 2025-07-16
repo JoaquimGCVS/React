b055345188959af12ed18be300a99a1004b6d190
JoaquimGCVS <jgui9292@gmail.com>
2025-07-16 19:07:31 -0300

    Instalando e aplicando react router

--- projetos/ola-mundo/src/App.js
+++ projetos/ola-mundo/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Inicio from './pages/Inicio';
+import SobreMim from './pages/SobreMim';
+
+console.log(window.location);
+
 function App() {
   return (
     <div>
-      <h1>Ola Mundo</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Inicio/>}></Route>
+          <Route path='/sobremim' element={<SobreMim/>}></Route>
+          <Route path='*' element={<div>Pagina nao encontrada</div>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
