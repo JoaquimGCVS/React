d58c62c9711770359c2a6d64a805096f3321aeb0
JoaquimGCVS <jgui9292@gmail.com>
2025-08-31 16:59:45 -0300

    projeto ola-mundo finalizado

--- projetos/ola-mundo/src/routes.js
+++ projetos/ola-mundo/src/routes.js
@@ -1,5 +1,8 @@
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Rodape from "componentes/Rodape";
+import ScrollToTop from "componentes/ScrollToTop";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import Post from "paginas/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from './paginas/Inicio';
@@ -8,6 +11,7 @@ import SobreMim from './paginas/SobreMim';
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
 
       <Routes>
@@ -16,7 +20,8 @@ function AppRoutes() {
           <Route path="sobremim" element={<SobreMim />} />
         </Route>
         
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        <Route path="posts/:id/*" element={<Post />} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
